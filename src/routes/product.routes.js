bb8cfda35cec95b6496d890480755e74d38b0018
ayusclg <freelancerayus@gmail.com>
2025-03-05 22:43:28 +0545

    Product All Done

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -6,8 +6,8 @@ const router = Router ()
 
         router.route("/create").post(verifyToken,productCreate)
         router.route("/fetch").get(productFetch)
-        router.route("/single/_id").get(fetchSingleProduct)
-        router.route("/update/_id").patch(verifyToken,updateProduct)
-        router.route("/uphoto/_id").patch(verifyToken,updatePhotoProduct)
+        router.route("/single/:_id").get(fetchSingleProduct)
+        router.route("/update/:_id").patch(verifyToken,updateProduct)
+        router.route("/uphoto/:_id").patch(verifyToken,updatePhotoProduct)
 
 export default router 
